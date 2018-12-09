bd5424f4ccad39face8d2a16220e9b441441191b
Lénaïc Grolleau <lenaic@lenaic.me>
2018-12-09 17:26:08 +0100

    Cast item price as integer

--- app/Item.php
+++ app/Item.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+    protected $_casts = [
+        'price' => 'integer'
+    ];
+
     public function users () {
         return $this->belongsToMany(User::class);
     }
